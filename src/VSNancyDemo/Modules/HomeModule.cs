00d8ffb218485a066db9e3e26d7dc164bd1f475a
Fernando Rodriguez <fernando@nandotech.com>
2016-09-29 00:02:35 -0400

    Initial commit with working modules & readme

---
+++ src/VSNancyDemo/Modules/HomeModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VSNancyDemo.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args => "Hello From Home Module.");
+            Get("/test", args => "Test Message.");
+        }
+    }
+}
---
+++ src/VSNancyDemo/Modules/ProductModules.cs
@@ -0,0 +1,43 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VSNancyDemo.Modules
+{
+    public class ProductModules : NancyModule
+    {
+        public static int nProductId = 1;
+        public static List<Product> lst = new List<Product>();
+        public ProductModules() : base("/products")
+        {
+            Get("/", args => GetProductList());
+            Get("/{id:int}", args => GetProductById(args.id));
+            Post("/create/{Name}", args =>
+            {
+                lst.Add(new Product() { Id = nProductId++, Name = args.Name });
+                return HttpStatusCode.OK;
+            });
+        }
+
+        public List<Product> GetProductList()
+        {
+            lst.Add(new Product() { Id = nProductId++, Name = "Bed" });
+            lst.Add(new Product() { Id = nProductId++, Name = "Table" });
+            lst.Add(new Product() { Id = nProductId++, Name = "Chair" });
+            return lst;
+        }
+
+        public Product GetProductById(int Id)
+        {
+            return lst.Find(item => item.Id == Id);
+        }
+
+        public class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
